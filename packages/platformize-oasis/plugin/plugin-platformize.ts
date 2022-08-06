a5bf69b77381fa10b593347d3c1ed8847ac6ab34
deepkolos <deepkolos@qq.com>
2022-08-06 22:44:40 +0800

    fix: oasis wechat image load error

--- packages/platformize-oasis/plugin/plugin-platformize.ts
+++ packages/platformize-oasis/plugin/plugin-platformize.ts
@@ -27,11 +27,20 @@ function patchOasis(): Plugin {
         );
 
         // 开发工具补丁, 但raycast还是不灵敏, 真机没问题
-        code = code.replace(
-          /activePointerCount === 1 &&/g,
-          `activePointerCount >= 1 && `,
-        );
+        code = code.replace(/activePointerCount === 1 &&/g, `activePointerCount >= 1 && `);
 
+        // 不适用texSubImage设置ImageSource, 微信小程序内会报错
+        code = code.replace(
+          `gl.texSubImage2D(this._target, mipLevel, x || 0, y || 0, baseFormat, dataType, imageSource);`,
+          `gl.texImage2D(this._target, mipLevel, baseFormat, baseFormat, dataType, imageSource);`,
+          // 并且下面的写法会crash...
+        //   `try {
+        //   // 微信小程序可能报错...
+        //   gl.texSubImage2D(this._target, mipLevel, x || 0, y || 0, baseFormat, dataType, imageSource);
+        // } catch (e) {
+        //   gl.texImage2D(this._target, mipLevel, baseFormat, baseFormat, dataType, imageSource);
+        // }`,
+        );
       }
       return { code, map: null };
     },
