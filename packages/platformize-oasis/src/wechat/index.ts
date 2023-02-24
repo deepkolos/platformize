76045af317ecc2ec8e349430a2a9ea3978a6aec8
deepkolos <deepkolos@qq.com>
2023-02-24 11:40:53 +0800

    feat: oasis adpate 0.8.3 to wechat

--- packages/platformize-oasis/src/wechat/index.ts
+++ packages/platformize-oasis/src/wechat/index.ts
@@ -10,5 +10,6 @@ export class WechatPlatform extends WechatPlatformBase {
     super(canvas, width, height);
     this.polyfill.$defaultWebGLExtensions = { OES_vertex_array_object: null };
     this.polyfill.HTMLCanvasElement = canvas.constructor as unknown as HTMLCanvasElement;
+    (canvas as any).focus = () => {};
   }
 }
