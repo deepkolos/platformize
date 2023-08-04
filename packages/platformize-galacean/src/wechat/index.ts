56642296b4b912eddb66133633166f593f160a05
jilin <jtabibito@qq.com>
2023-08-04 09:14:54 +0800

    fix: pointerleave and pointercancel not call while get touchend and touchcancel

--- packages/platformize-galacean/src/wechat/index.ts
+++ packages/platformize-galacean/src/wechat/index.ts
@@ -2,12 +2,13 @@
 /// <reference types="@types/offscreencanvas" />
 
 import { WechatPlatform as WechatPlatformBase } from 'platformize';
-import $Blob from '../base/Blob';
+import { Touch, TouchEvent } from 'platformize/dist/base/EventTarget';
+import $Blob from 'platformize/dist/base/Blob';
 import { createImage, createVideoContext } from '../base/utils/helper';
 import { $FontFaceSet } from '../base/Font';
 import type { Polyfill } from '../Platform';
 
-// 微信小程序创建离屏画布api变成了createOffScreenCanvas
+// 微信小游戏创建离屏画布api变成了createOffScreenCanvas
 function OffscreenCanvas() {
   // @ts-ignore
   if (wx.createOffscreenCanvas === undefined)
@@ -42,5 +43,65 @@ export class WechatPlatform extends WechatPlatformBase {
     this.polyfill.$defaultWebGLExtensions = { OES_vertex_array_object: null };
     this.polyfill.HTMLCanvasElement = canvas.constructor as unknown as HTMLCanvasElement;
     (canvas as any).focus = () => {};
+  }
+
+  dispatchTouchEvent(
+    e: TouchEvent = {
+      touches: [],
+      changedTouches: [],
+      timeStamp: 0,
+      type: '',
+    },
+  ) {
+    const target = { ...this };
+    // 微信小程序type会多on
+    const type = e.type.replace('on', '');
+    const changedTouches = e.changedTouches.map(touch => new Touch(touch));
+
+    const event = {
+      changedTouches: changedTouches,
+      touches: e.touches.map(touch => new Touch(touch)),
+      targetTouches: Array.prototype.slice.call(e.touches.map(touch => new Touch(touch))),
+      timeStamp: e.timeStamp,
+      target: target,
+      currentTarget: target,
+      type,
+      cancelBubble: false,
+      cancelable: false,
+    };
+
+    this.canvas.dispatchEvent(event);
+
+    if (changedTouches.length) {
+      const touch = changedTouches[0];
+      const pointerEvent = {
+        clientX: touch.clientX,
+        clientY: touch.clientY,
+        pageX: touch.pageX,
+        pageY: touch.pageY,
+        offsetX: touch.pageX,
+        offsetY: touch.pageY,
+        pointerId: touch.identifier,
+        // to fix oasis controls https://www.w3.org/TR/uievents/#dom-mouseevent-buttons
+        buttons: 1,
+        type:
+          {
+            touchstart: 'pointerdown',
+            touchmove: 'pointermove',
+            touchend: 'pointerup',
+            touchcancel: 'pointercancel',
+          }[type] || '',
+        pointerType: 'touch',
+      };
+
+      this.canvas.dispatchEvent(pointerEvent);
+      // call pointerleave if touchend after pointerup.
+      if (type === 'touchend') {
+        this.canvas.dispatchEvent({
+          ...pointerEvent,
+          type: 'pointerleave'
+        });
+      }
+    }
   }
 }
